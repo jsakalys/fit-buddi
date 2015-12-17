8a8255616e68fc7ac2738a93e1de8ccee867d63c
JSAKALYS <TASHIKANAKOTO@GMAIL.COM>
2015-12-17 14:21:51 -0800

    debugging

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -154,10 +154,17 @@ angular.module('fitBuddi.controllers', [])
 }])
 
 .controller('HomeCtrl', ['$scope', 'currentAuth', function($scope, currentAuth){
-  var usersRef = new Firebase("https://fitbuddi.firebaseio.com/users");
-  // get current user info
-  usersRef.child(currentAuth.uid).on("value", function(user){
-    $scope.currentUser = user.val();
+  // $scope.$on('$ionicView.afterEnter', function(){
+    var usersRef = new Firebase("https://fitbuddi.firebaseio.com/users");
+    // get current user info
+    usersRef.child(currentAuth.uid).on("value", function(user){
+      $scope.currentUser = user.val();
+      $scope.fullHearts = $scope.currentUser.buddi.health;
+      $scope.emptyHearts = (3 - $scope.fullHearts);
+      $scope.mood = $scope.currentUser.buddi.mood;
+    }, function (errorObject) {
+      alert("Sorry! There was an error getting your data:" + errorObject.code);
+    });
     $scope.getHealth = function(num){
       return new Array(num);   
     };
@@ -168,66 +175,64 @@ angular.module('fitBuddi.controllers', [])
         return false;
       };
     };
-    $scope.fullHearts = $scope.currentUser.buddi.health;
-    $scope.emptyHearts = (3 - $scope.fullHearts);
-    $scope.mood = $scope.currentUser.buddi.mood;
-  }, function (errorObject) {
-    alert("Sorry! There was an error getting your data:" + errorObject.code);
-  });
-  $scope.giveGift = function(){
-    usersRef.child(currentAuth.uid).child('buddi').child('mood').set(
-      'happy'
-    );
-  };
-  $('#pet').sprite({fps: 4, no_of_frames: 7});
-  $('#pet').isDraggable({
-    start: function() {
-    },
-    stop: function() {
-      $('#pet').spState(7);
-    },
-    drag: function() {
-      $('#pet').spState(4);
-    }
-  });
-  stepcounter.getTodayStepCount(function(success){
-    $scope.stepsToday = success;
-    // $scope.stepsToday = 8000
-    if ($scope.stepsToday < 1000) {
-      $('#pet').spState(1);
-      $scope.texttyping = [
-        "Zzz^500.^500.^500.^500 Zzzzzz.^500.^500.^500"
-      ]
-    } else if ($scope.stepsToday < 5000) {
-      usersRef.child(currentAuth.uid).child('buddi').child('health').set(1);
-      usersRef.child(currentAuth.uid).child('buddi').child('mood').set('unhappy');
-      $('#pet').spState(5);
-      $scope.texttyping = [
-        "Hey! ^500 I noticed you haven't walked that much today yet^500.^500.^500.^500<br>How about we go outside and see the sights?"
-      ]
-    } else if ($scope.stepsToday < 10000) {
-      usersRef.child(currentAuth.uid).child('buddi').child('health').set(2);
-      usersRef.child(currentAuth.uid).child('buddi').child('mood').set('stable');
-      $('#pet').spState(6);
-      $scope.texttyping = [
-        "Wow! ^500.^500.^500.^500You've walked quite a bit today.^1000<br>Let's see if we can get to 10,000 steps!"
-      ]
-    } else if ($scope.stepsToday >= 10000) {
-      usersRef.child(currentAuth.uid).child('buddi').child('health').set(3);
-      usersRef.child(currentAuth.uid).child('buddi').child('mood').set('happy');
-      $('#pet').spState(3);
-      $scope.texttyping = [
-        "Holy cow! ^500 I'd say we've put in quite a workout today! :sweat:"
-      ];
-    } else {
-      $('#pet').spState(1);
-      $scope.texttyping = [
-        "Zzz^500.^500.^500.^500 Zzzzzz.^500.^500.^500"
-      ]
-    }
-  },function(failure){
-    alert(failure)
-  });
+    $scope.giveGift = function(){
+      usersRef.child(currentAuth.uid).child('buddi').child('mood').set(
+        'happy'
+      );
+    };
+    $(function() {
+      $('#pet').sprite({fps: 4, no_of_frames: 7});
+      $('#pet').isDraggable({
+        start: function() {
+        },
+        stop: function() {
+          $('#pet').spState(7);
+        },
+        drag: function() {
+          $('#pet').spState(4);
+        }
+      });
+    });
+    stepcounter.getTodayStepCount(function(success){
+      alert(success);
+      stepsToday = success;
+      // stepsToday = 8000
+      if (stepsToday < 1000) {
+        $('#pet').spState(1);
+        $scope.texttyping = [
+          "Zzz^500.^500.^500.^500 Zzzzzz.^500.^500.^500"
+        ]
+      } else if (stepsToday < 5000) {
+        usersRef.child(currentAuth.uid).child('buddi').child('health').set(1);
+        usersRef.child(currentAuth.uid).child('buddi').child('mood').set('unhappy');
+        $('#pet').spState(5);
+        $scope.texttyping = [
+          "Hey! ^500 I noticed you haven't walked that much today yet^500.^500.^500.^1000 How about we go outside and see the sights?"
+        ]
+      } else if (stepsToday < 10000) {
+        usersRef.child(currentAuth.uid).child('buddi').child('health').set(2);
+        usersRef.child(currentAuth.uid).child('buddi').child('mood').set('stable');
+        $('#pet').spState(6);
+        $scope.texttyping = [
+          "Wow! ^500.^500.^500.^500You've walked quite a bit today.^1000 Let's see if we can get to 10,000 steps!"
+        ]
+      } else if (stepsToday >= 10000) {
+        usersRef.child(currentAuth.uid).child('buddi').child('health').set(3);
+        usersRef.child(currentAuth.uid).child('buddi').child('mood').set('happy');
+        $('#pet').spState(3);
+        $scope.texttyping = [
+          "Holy cow! ^500 I'd say we've put in quite a workout today! :sweat:"
+        ];
+      } else {
+        $('#pet').spState(1);
+        $scope.texttyping = [
+          "Zzz^500.^500.^500.^500 Zzzzzz.^500.^500.^500"
+        ]
+      }
+    },function(failure){
+      alert(failure)
+    });
+  // });
 }])
 
 .controller('StatsCtrl', ['$scope', 'currentAuth', function($scope, currentAuth){
@@ -239,86 +244,75 @@ angular.module('fitBuddi.controllers', [])
     }, function (errorObject) {
       alert("Sorry! There was an error getting your data:" + errorObject.code);
     });
-    $scope.stepHistory = {
-      "2015-01-01":{"offset": 123, "steps": 456},
-      "2015-01-02":{"offset": 579, "steps": 789},
-      "2015-01-03":{"offset": 579, "steps": 1034},
-      "2015-01-04":{"offset": 579, "steps": 2345},
-      "2015-01-05":{"offset": 579, "steps": 456},
-      "2015-01-06":{"offset": 579, "steps": 788},
-      "2015-01-07":{"offset": 579, "steps": 3645},
-      "2015-01-08":{"offset": 579, "steps": 5678},
-      "2015-01-09":{"offset": 579, "steps": 3454},
-      "2015-01-10":{"offset": 579, "steps": 1233}
-    };
+    // $scope.stepHistory = { // sample data
+    //   "2015-01-01":{"offset": 123, "steps": 456},
+    //   "2015-01-02":{"offset": 579, "steps": 789},
+    //   "2015-01-03":{"offset": 579, "steps": 1034},
+    //   "2015-01-04":{"offset": 579, "steps": 2345},
+    //   "2015-01-05":{"offset": 579, "steps": 456},
+    //   "2015-01-06":{"offset": 579, "steps": 788},
+    //   "2015-01-07":{"offset": 579, "steps": 3645},
+    //   "2015-01-08":{"offset": 579, "steps": 5678},
+    //   "2015-01-09":{"offset": 579, "steps": 3454},
+    //   "2015-01-10":{"offset": 579, "steps": 1233}
+    // };
     $scope.getKey = function(obj, idx){
       return Object.keys(obj)[idx]
     };
-    // stepcounter.getTodayStepCount(function(success){
-    //   $scope.stepsToday = success
-    // },function(failure){
-    //   alert(failure)
-    // });
-    // stepcounter.getStepCount(function(success){
-    //   $scope.totalSteps = success
-    // },function(failure){
-    //   alert(failure)
-    // });
-    // stepcounter.getHistory(function(success){
-    //   $scope.stepHistory = success
-    // },function(failure){
-    //   alert(failure)
-    // });
-    // *bar chart stuff* //
-    var stepLabels = [];
-    var stepData = [];
-    var dataLimit = 7;
-    var i = 0;
-    angular.forEach($scope.stepHistory, function(data, key){
-      if (i == dataLimit) {
-        return;
-      } else {
-        stepLabels.push(key);
-        stepData.push(data.steps);
-        i++
-      }
-    });
-    
-    var data = {
-      labels: stepLabels,
-      datasets: [
-        {
-          label: "My First dataset",
-          fillColor: "rgba(220,220,220,0.5)",
-          strokeColor: "rgba(220,220,220,0.8)",
-          highlightFill: "rgba(220,220,220,0.75)",
-          highlightStroke: "rgba(220,220,220,1)",
-          data: stepData
+    stepcounter.getHistory(function(success){
+      $scope.stepHistory = success
+      // *bar chart stuff* //
+      var stepLabels = [];
+      var stepData = [];
+      var dataLimit = 7;
+      var i = 0;
+      angular.forEach($scope.stepHistory, function(data, key){
+        if (i == dataLimit) {
+          return;
+        } else {
+          stepLabels.push(key);
+          stepData.push(data.steps);
+          i++
         }
-      ]
-    };
-    var options = {
-      scaleBeginAtZero : true,
-      scaleShowGridLines : false,
-      scaleGridLineColor : "rgba(0,0,0,.05)",
-      scaleGridLineWidth : 1,
-      scaleShowHorizontalLines: true,
-      scaleShowVerticalLines: true,
-      scaleFontSize: 10,
-      scaleFontFamily: 'Half-Bold-Pixel-7',
-      scaleLineWidth: 0,
-      tooltipFontFamily: 'Half-Bold-Pixel-7',
-      tooltipFontSize: 10,
-      tooltipFontStyle: 'normal',
-      responsive: true,
-      barShowStroke : false,
-      barStrokeWidth : 1,
-      barValueSpacing : 2,
-      barDatasetSpacing : 1,
-      legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].fillColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
-    };
-    var ctx = document.getElementById("stepChart").getContext("2d");
-    var stepChart = new Chart(ctx).Bar(data, options);
+      });
+      var data = {
+        labels: stepLabels,
+        datasets: [
+          {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.5)",
+            strokeColor: "rgba(220,220,220,0.8)",
+            highlightFill: "rgba(220,220,220,0.75)",
+            highlightStroke: "rgba(220,220,220,1)",
+            data: stepData
+          }
+        ]
+      };
+      var options = {
+        scaleBeginAtZero : true,
+        scaleShowGridLines : false,
+        scaleGridLineColor : "rgba(0,0,0,.05)",
+        scaleGridLineWidth : 1,
+        scaleShowHorizontalLines: true,
+        scaleShowVerticalLines: true,
+        scaleFontSize: 10,
+        scaleFontFamily: 'Half-Bold-Pixel-7',
+        scaleLineWidth: 0,
+        tooltipFontFamily: 'Half-Bold-Pixel-7',
+        tooltipFontSize: 10,
+        tooltipFontStyle: 'normal',
+        responsive: true,
+        barShowStroke : false,
+        barStrokeWidth : 1,
+        barValueSpacing : 2,
+        barDatasetSpacing : 1,
+        legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].fillColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+      };
+      var ctx = document.getElementById("stepChart").getContext("2d");
+      var stepChart = new Chart(ctx).Bar(data, options);
+    },function(failure){
+      alert(failure)
+    });
   });
 }])
 
@@ -362,9 +356,9 @@ angular.module('fitBuddi.controllers', [])
         backSpeed: 0,
         cursorChar: " :"
       };
-     // $(function() {
+      $(function() {
         $(document.getElementById("typed-output")).typed(options);
-      //});
+      });
     }
   };
 });
